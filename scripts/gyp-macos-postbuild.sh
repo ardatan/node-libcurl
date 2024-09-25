f7c96f2736b883df527d0abd777f296505e19bce
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-09-25 21:26:08 +0300

    Fix cURL test

--- scripts/gyp-macos-postbuild.sh
+++ scripts/gyp-macos-postbuild.sh
@@ -3,5 +3,6 @@
 if [ -f `curl-config --prefix`/lib/libcurl.dylib ]; then
     install_name_tool -change $(otool -D `curl-config --prefix`/lib/libcurl.dylib | sed -n 2p) @rpath/libcurl.dylib $1
 else
-    echo "File does not exist. Skipping postbuild steps. This means libcurl binaries were not found"
+    echo "File does not exist. This means libcurl binaries were not found"
+    exit 1
 fi
